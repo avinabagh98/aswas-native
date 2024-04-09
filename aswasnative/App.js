635bc6e46788b64d49924a1bf13ea299d3934ac0
Avinaba Ghosh <75163712+avinabagh98@users.noreply.github.com>
2024-04-10 01:06:46 +0530

    initial commit

---
+++ aswasnative/App.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {View, Text} from 'react-native';
+import {WebView} from 'react-native-webview';
+
+const App = () => {
+  return (
+    <WebView
+      source={{uri: 'https://aswaas.ongoingprojects.xyz/'}}
+      style={{flex: 1}}
+    />
+  );
+};
+
+export default App;
